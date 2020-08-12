c90ecc6a671b5199ef88e4c19ca87198ee2950f1
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-08-12 19:06:23 +0500

    get and fetch data from jsonplaceholder.com

---
+++ bootcamp.js
@@ -0,0 +1,34 @@
+console.log("fetch API")
+
+async function getApi()
+{
+    const api = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    console.log(api, "API")
+    const json = await api.json()
+    console.log(json, "JSON")
+}
+
+// getApi()
+
+async function postAPI()
+{
+    var posrURL = "https://jsonplaceholder.typicode.com/posts"
+    var data = {
+        method : 'POST',
+        body : JSON.stringify({
+            title : 'bootcamp',
+            body : 'javascript',
+            userId : 1
+        }),
+        headers : {
+            "Content-type" : "application/json; charset=UTF-8"
+        }
+    }
+    const api = await fetch(posrURL, data)
+
+    console.log(api, "API")
+    const json = await api.json()
+    console.log(json, "JSON")
+}
+
+postAPI()
